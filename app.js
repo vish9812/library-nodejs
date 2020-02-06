839c94fbf0050e6087f32a974a4090469598001d
Vishal <vish9812@gmail.com>
2020-02-07 02:48:30 +0530

    First Commit - Add one route and its page

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const commonData = {
+  nav: [
+    {
+      link: '/books',
+      title: 'Books',
+    },
+    {
+      link: '/authors',
+      title: 'Authors',
+    }],
+  title: 'Library',
+};
+const bookRouter = require('./src/routes/bookRoutes')(commonData);
+
+app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.use('/books', bookRouter);
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.redirect('/books');
+});
+
+app.listen(port, () => {
+  debug(`Listening on ${chalk.green(port)}`);
+});
